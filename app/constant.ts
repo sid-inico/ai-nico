3a9ee1819ce21b206328b3e379f7445900fd3ef6
Alejandro Galache <axgalache@gmail.com>
2024-05-21 19:18:06 +0200

    Fix build

--- app/constant.ts
+++ app/constant.ts
@@ -183,7 +183,14 @@ const googleModels = [
   "gemini-pro-vision",
 ];
 
-const anthropicModels = [];
+const anthropicModels = [
+  "claude-instant-1.2",
+  "claude-2.0",
+  "claude-2.1",
+  "claude-3-sonnet-20240229",
+  "claude-3-opus-20240229",
+  "claude-3-haiku-20240307",
+];
 
 export const DEFAULT_MODELS = [
   ...openaiModels.map((name) => ({
